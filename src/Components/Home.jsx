283e2cfc5b6153aa332a1e123e2ac7002dcb47db
vaibhav-hundekar <127763706+vaibhav-hundekar@users.noreply.github.com>
2023-09-08 22:01:57 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,12 +21,12 @@ const Home = () => {
 
           <div>
             <Link
-              to="portfolio"
+              to="skills"
               smooth
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500  cursor-pointer"
             >
-              Portfolio
+              Skills
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
